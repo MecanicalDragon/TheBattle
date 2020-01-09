dc8391a6c1abb4c7b06fa0d7e42e2d0ea31e7a35
stretyak <Stanislav.Tretyakov@t-systems.com>
2020-01-09 09:38:58 +0300

    BattleService started

--- src/main/kotlin/net/medrag/theBattle/service/BattleSearchingService.kt
+++ src/main/kotlin/net/medrag/theBattle/service/BattleSearchingService.kt
@@ -1,12 +1,14 @@
 package net.medrag.theBattle.service
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import net.medrag.theBattle.model.classes.Squad
 import net.medrag.theBattle.model.squad.FoesPair
 import org.springframework.stereotype.Service
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentLinkedQueue
-import java.util.concurrent.atomic.AtomicBoolean
 
 
 /**
@@ -18,22 +20,24 @@ class BattleSearchingService {
 
     companion object {
         private val searching = ConcurrentLinkedQueue<Squad>()
-        private var searchingProcess: AtomicBoolean = AtomicBoolean(false)  // delete
         private var battleFoes = ConcurrentHashMap<UUID, FoesPair>()
 
-        private fun lookingForFoes() {
-            while(searchingProcess.get()){
+        private val searchingJob = GlobalScope.async {
+            while (true) {
 
                 var foe1: Squad? = null
                 var foe2: Squad? = null
 
-                while(foe1 == null){
-                    //pause coroutine
+                while (foe1 == null) {
                     foe1 = searching.poll()
-                }
-                while(foe2 == null){
                     //pause coroutine
+                    if (foe1 == null) delay(1000)
+                }
+                //TODO: need better solution
+                while (foe2 == null) {
                     foe2 = searching.poll()
+                    //pause coroutine
+                    if (foe2 == null) delay(1000)
                 }
                 val uuid = UUID.randomUUID()
                 val pair = FoesPair(uuid, foe1, foe2)
@@ -43,26 +47,19 @@ class BattleSearchingService {
         }
 
         init {
-            lookingForFoes()
+            searchingJob.start()
         }
     }
-
 
     fun add(squad: Squad) {
-        searching.add(squad)
-        //unpause coroutine
-        if (!searchingProcess.get() && searching.size > 1) {
-            searchingProcess.compareAndSet(false, true)
-            lookingForFoes()
+        val foe = searching.poll()
+        if (foe == null) {
+            searching.add(squad)
+            //unpause coroutine
+        } else {
+            val uuid = UUID.randomUUID()
+            val pair = FoesPair(uuid, foe, squad)
+            battleFoes[uuid] = pair
         }
-    }
-
-    //remove method
-    fun poll(): Squad {
-        val squad = searching.poll()
-        if (searching.size < 2 && searchingProcess.get()) {
-            searchingProcess.compareAndSet(true, false)
-        }
-        return squad
     }
 }
